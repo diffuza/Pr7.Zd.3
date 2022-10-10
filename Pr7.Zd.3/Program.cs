12b3690166bc4c25d294a5a3959f809b6f572f94
USER <USER@DESKTOP-TRQG01D>
2022-10-10 20:20:58 +0300

    Добавьте файлы проекта.

---
+++ Pr7.Zd.3/Program.cs
@@ -0,0 +1,21 @@
+﻿//Известен объем и масса тела. Определить плотность материала этого тела.
+using System;
+using System;
+namespace HelloApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double V, m, p;
+            Console.WriteLine("Введите значение массы (m): ");
+            m = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение объема(V): ");
+            V = Convert.ToDouble(Console.ReadLine());
+            p = m / V;
+            Console.WriteLine("Плотность материала этого тела равна: {0}", Math.Round(p,4));
+            Console.ReadLine();
+
+        }
+    }
+}
